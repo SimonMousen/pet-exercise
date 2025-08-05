a963b11b948d869595b00a1a827d4a1370c35d13
SimonMousen <simonuwusierra@gmail.com>
2025-08-05 18:47:27 -0500

    Funcion mascota

---
+++ index.js
@@ -0,0 +1,6 @@
+function getPetExerciseInfo(mascota, edad){
+    if (typeof mascota !== 'string') {
+        return 'Tipo de mascota invalida';
+    }
+    const tipoMascota = mascota.toLowerCase();
+}

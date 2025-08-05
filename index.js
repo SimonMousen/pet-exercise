549aa990ea970e892413bf652eee602ce51751ed
SimonMousen <simonuwusierra@gmail.com>
2025-08-05 18:58:04 -0500

    Logica 1

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function getPetExerciseInfo(mascota, edad){
+function getPetExerciseInfo(mascota, edad) {
     if (typeof mascota !== 'string') {
         return 'Tipo de mascota invalida';
     }
     const tipoMascota = mascota.toLowerCase();
+
+    if (tipoMascota === 'perro') {
+        if (edad < 1) {
+            return 'Los cachorros necesitan pequeñas y frecuentes sesiones de juego.';
+        } else if (edad >= 1 && edad <= 7) {
+            return 'Los perros a esta edad necesitan caminatas diarias y sesiones de juego.';
+        } else {
+            return 'Los perros viejos necesitan caminatas mas cortas.';
+        }
+    }
 }
